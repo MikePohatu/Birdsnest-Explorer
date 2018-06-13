99dfdb6b75ec3b9e169cfa903b39c8296ec06fda
Mike Pohatu <mikepohatu@gmail.com>
2018-06-13 21:32:03 +1200

    initial configuration load options

--- source/BirdsNest.Net/ADScanner/Program.cs
+++ source/BirdsNest.Net/ADScanner/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace ADScanner
 {
     class Program
     {
+        private Configuration _config;
+
         static void Main(string[] args)
         {
+            string configfile = string.Empty;
+
+            foreach (string arg in args)
+            {
+                string[] param = arg.Split(new[] { ":" }, 2, StringSplitOptions.None);
+                Debug.WriteLine(param[0] + " " + param[1]);
+                switch (param[0].ToUpper())
+                {
+                    case "/CONFIG":
+                        configfile = param[1];
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void LoadConfig(string configfile)
+        {
+            try
+            {
+                this._config = FileHandler.ReadConfigurationFromFile(configfile);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error loading configuration file: " + e.Message);
+                Environment.Exit(1001);
+            }
         }
     }
 }
