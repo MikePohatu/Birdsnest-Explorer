e710f07da16a3c7e616f35dd5e6dedc8364ac283
Mike Pohatu <mikepohatu@gmail.com>
2018-06-29 15:00:35 +1200

    started custom application scanner

---
+++ source/BirdsNest.Net/CustomAppScanner/Types.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomAppScanner
+{
+    internal static class Types
+    {
+        //Nodes - use upper case
+        public static string Application { get { return "APPLICATION"; } }
+
+        //edges - use camel case
+
+    }
+}
--- source/BirdsNest.Net/common/CommonTypes.cs
+++ source/BirdsNest.Net/common/CommonTypes.cs
@@ -2,10 +2,15 @@
 {
     public static class CommonTypes
     {
+        //Nodes - use upper case
+        public static string Orphaned { get { return "ORPHANED"; } }
+        public static string Device { get { return "DEVICE"; } }
+
+        //edges - use camel case
         public static string Uses { get { return "Uses"; } }
         public static string GivesAccessTo { get { return "GivesAccessTo"; } }
         public static string ConnectedTo { get { return "ConnectedTo"; } }
-        public static string Orphaned { get { return "ORPHANED"; } }
-        public static string Device { get { return "DEVICE"; } }
+        public static string WritesDataTo { get { return "WritesDataTo"; } }
+        public static string ReadsDataFrom { get { return "ReadsDataFrom"; } }
     }
 }
