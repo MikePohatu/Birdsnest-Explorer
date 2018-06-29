39c92e8ecab3978e810b60e7b3c616d7d2c17f85
Mike Pohatu <mikepohatu@gmail.com>
2018-06-30 11:10:36 +1200

    menu mostly working

--- source/web/menu.js
+++ source/web/menu.js
@@ -1,11 +1,21 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
-	document.getElementById("mySidenav").style.width = "250px";
-	document.getElementById("main").style.marginLeft = "250px";
+	document.getElementById("openclosebtn").onclick = function() { 
+            closeNav(); 
+        };
+	document.getElementById("openclosebtn").innerHTML = "&lt;";
+	document.getElementById("sidemenu").style.width = "250px";
+	document.getElementById("openclosebar").style.left = "250px";
+	document.getElementById("main").style.left = "280px";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
-	document.getElementById("mySidenav").style.width = "0";
-	document.getElementById("main").style.marginLeft = "0";
+	document.getElementById("openclosebtn").onclick = function() { 
+            openNav(); 
+        };
+	document.getElementById("openclosebtn").innerHTML = "&gt;";
+	document.getElementById("sidemenu").style.width = "0";
+	document.getElementById("openclosebar").style.left = "0";
+	document.getElementById("main").style.left = "0";
 }
