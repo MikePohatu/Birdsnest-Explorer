c4a5fbddeb9cd402aa1adb2c6f8382f0005736d8
Mike Pohatu <mikepohatu@gmail.com>
2018-07-08 23:07:07 +1200

    mainly d3 changes. Basic drawing with icon working

--- source/web/js/draw-d3.js
+++ source/web/js/draw-d3.js
@@ -1,54 +1,51 @@
 function DrawGraph(selectid) {
-	var neo4jd3 = new Neo4jd3('#'+selectid, {
-        neo4jDataUrl: 'json/neo4jd4sample-neo4j.json',
-		highlight: [
-			{
-				class: 'Project',
-				property: 'name',
-				value: 'neo4jd3'
-			}, {
-				class: 'User',
-				property: 'userId',
-				value: 'eisman'
-			}
-		],
-		icons: {
-			'Api': 'gear',
-			'BirthDate': 'birthday-cake',
-			'Cookie': 'paw',
-			'Email': 'at',
-			'Git': 'git',
-			'Github': 'github',
-			'Ip': 'map-marker',
-			'Issues': 'exclamation-circle',
-			'Language': 'language',
-			'Options': 'sliders',
-			'Password': 'asterisk',
-			'Phone': 'phone',
-			'Project': 'folder-open',
-			'SecurityChallengeAnswer': 'commenting',
-			'User': 'user',
-			'zoomFit': 'arrows-alt',
-			'zoomIn': 'search-plus',
-			'zoomOut': 'search-minus'
-		},
-		minCollision: 60,
-		nodeRadius: 25,
-		onNodeDoubleClick: function(node) {
-			switch(node.id) {
-				case '25':
-					// Google
-					window.open(node.properties.url, '_blank');
-					break;
-				default:
-					var maxNodes = 5,
-						data = neo4jd3.randomD3Data(node, maxNodes);
-					neo4jd3.updateWithD3Data(data);
-					break;
-			}
-		},
-		zoomFit: true
-	});
-    
-    console.log(neo4jd3.size());
+	var defaultsize = 40;
+	var defaultstroke = 3;
+
+	var vis = d3.select("#"+selectid)
+            .append("svg");
+
+    var nodes = [{x: 30, y: 50, scaling:1},
+              {x: 300, y: 110, scaling:1.5},
+              {x: 150, y: 200, scaling:0.6}];
+
+	var links = [
+		{source: nodes[0], target: nodes[1]},
+		{source: nodes[2], target: nodes[1]}
+		];
+
+	vis.selectAll(".line")
+		.data(links)
+		.enter()
+		.append("line")
+			.attr("x1", function(d) { return (d.source.x + ((defaultsize*d.source.scaling))/2)})
+			.attr("y1", function(d) { return (d.source.y + ((defaultsize*d.source.scaling))/2) })
+			.attr("x2", function(d) { return (d.target.x + ((defaultsize*d.target.scaling))/2)})
+			.attr("y2", function(d) { return (d.target.y + ((defaultsize*d.target.scaling))/2) })
+			.style("stroke", "rgb(6,120,155)");
+
+	g = vis.selectAll(".nodes")
+		.data(nodes)
+		.enter()
+			.append("g")
+				.attr("transform",function(d) { 
+						return "translate(" + d.x + "," +d.y+")"
+					})
+				.attr("class","nodes");
+
+	g.append("svg:circle")
+		.attr("r", function(d) { return ((defaultsize * d.scaling)/2) + "px" })
+		.attr("cx", function(d) { return ((defaultsize * d.scaling)/2) })
+		.attr("cy", function(d) { return ((defaultsize * d.scaling)/2) })
+		.style("stroke-width", function(d) { return (defaultstroke * d.scaling) + "px" })
+		.attr("fill", "yellow")
+		.style("stroke", "orange");
+
+	g.append("i")
+		.attr("height", function(d) { return ((defaultsize * d.scaling) * 0.6) + "px" })
+		.attr("width", function(d) { return ((defaultsize * d.scaling) * 0.6) + "px" })
+		.attr("x", function(d) { return ((defaultsize * d.scaling) * 0.2) })
+		.attr("y",function(d) { return ((defaultsize * d.scaling) * 0.2)})
+		.attr("class","fas fa-user")
+		.attr("color","orange"); 
 }
