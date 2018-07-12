0358812662b8e97233bc4c6c2831575c22b8e8b0
Mike Pohatu <mikepohatu@gmail.com>
2018-07-13 10:38:37 +1200

    arrow heads working correctly

--- source/web/js/draw-d3.js
+++ source/web/js/draw-d3.js
@@ -1,23 +1,23 @@
 var nodedata = [
-	{name:"Node0", scaling:1, class:"fas fa-user", color:"orange", selcolor:"lightgreen", fill: "yellow"},
-	{name:"Node1", scaling:1, class:"fas fa-user", color:"orange", selcolor:"lightgreen", fill: "yellow"},
-	{name:"Node2", scaling:1, class:"fas fa-user", color:"red", selcolor:"lightgreen", fill: "pink"},
-	{name:"Node3", scaling:1, class:"fas fa-user", color:"green", selcolor:"lightgreen", fill: "lightgreen"},
-	{name:"Node4", scaling:1, class:"fas fa-user", color:"orange", selcolor:"lightgreen", fill: "yellow"},
-	{name:"Node5", scaling:1, class:"fas fa-user", color:"red", selcolor:"lightgreen", fill: "pink"},
-	{name:"Node6", scaling:1, class:"fas fa-user", color:"green", selcolor:"lightgreen", fill: "lightgreen"},
-	{name:"Node7", scaling:1, class:"fas fa-user", color:"orange", selcolor:"lightgreen", fill: "yellow"},
-	{name:"Node8", scaling:1, class:"fas fa-user", color:"red", selcolor:"lightgreen", fill: "pink"},
-	{name:"Node9", scaling:1, class:"fas fa-user", color:"green", selcolor:"lightgreen", fill: "lightgreen"},
-	{name:"Node10", scaling:1, class:"fas fa-user", color:"orange", selcolor:"lightgreen", fill: "yellow"},
-	{name:"Node11", scaling:1, class:"fas fa-user", color:"red", selcolor:"lightgreen", fill: "pink"},
-	{name:"Node12", scaling:1, class:"fas fa-user", color:"green", selcolor:"lightgreen", fill: "lightgreen"},
-	{name:"Node13", scaling:1, class:"fas fa-user", color:"orange", selcolor:"lightgreen", fill: "yellow"},
-	{name:"Node14", scaling:1, class:"fas fa-user", color:"red", selcolor:"lightgreen", fill: "pink"},
-	{name:"Node15", scaling:1, class:"fas fa-user", color:"green", selcolor:"lightgreen", fill: "lightgreen"},
-	{name:"Node16", scaling:1, class:"fas fa-user", color:"orange", selcolor:"lightgreen", fill: "yellow"},
-	{name:"Node17", scaling:1.5, class:"fas fa-user", color:"red", selcolor:"lightgreen", fill: "pink"},
-	{name:"Node18", scaling:1, class:"fas fa-user", color:"green", selcolor:"lightgreen", fill: "lightgreen"}
+	{name:"Node0", relatedcount:1, class:"fas fa-user", color:"orange", selcolor:"lightgreen", fill: "yellow"},
+	{name:"Node1", relatedcount:1, class:"fas fa-user", color:"orange", selcolor:"lightgreen", fill: "yellow"},
+	{name:"Node2", relatedcount:1, class:"fas fa-user", color:"red", selcolor:"lightgreen", fill: "pink"},
+	{name:"Node3", relatedcount:1, class:"fas fa-user", color:"green", selcolor:"lightgreen", fill: "lightgreen"},
+	{name:"Node4", relatedcount:1, class:"fas fa-user", color:"orange", selcolor:"lightgreen", fill: "yellow"},
+	{name:"Node5", relatedcount:1, class:"fas fa-user", color:"red", selcolor:"lightgreen", fill: "pink"},
+	{name:"Node6", relatedcount:1, class:"fas fa-user", color:"green", selcolor:"lightgreen", fill: "lightgreen"},
+	{name:"Node7", relatedcount:1, class:"fas fa-user", color:"orange", selcolor:"lightgreen", fill: "yellow"},
+	{name:"Node8", relatedcount:1, class:"fas fa-user", color:"red", selcolor:"lightgreen", fill: "pink"},
+	{name:"Node9", relatedcount:1, class:"fas fa-user", color:"green", selcolor:"lightgreen", fill: "lightgreen"},
+	{name:"Node10", relatedcount:1, class:"fas fa-user", color:"orange", selcolor:"lightgreen", fill: "yellow"},
+	{name:"Node11", relatedcount:1, class:"fas fa-user", color:"red", selcolor:"lightgreen", fill: "pink"},
+	{name:"Node12", relatedcount:1, class:"fas fa-user", color:"green", selcolor:"lightgreen", fill: "lightgreen"},
+	{name:"Node13", relatedcount:1, class:"fas fa-user", color:"orange", selcolor:"lightgreen", fill: "yellow"},
+	{name:"Node14", relatedcount:1, class:"fas fa-user", color:"red", selcolor:"lightgreen", fill: "pink"},
+	{name:"Node15", relatedcount:1, class:"fas fa-user", color:"green", selcolor:"lightgreen", fill: "lightgreen"},
+	{name:"Node16", relatedcount:1, class:"fas fa-user", color:"orange", selcolor:"lightgreen", fill: "yellow"},
+	{name:"Node17", relatedcount:1.5, class:"fas fa-user", color:"red", selcolor:"lightgreen", fill: "pink"},
+	{name:"Node18", relatedcount:1, class:"fas fa-user", color:"green", selcolor:"lightgreen", fill: "lightgreen"}
 ];
 
 var linkdata = [
@@ -47,14 +47,25 @@ function drawGraph(selectid) {
 
 	var defaultsize = 40;
 	var defaultstroke = 3;
+	var totalnodesize = (defaultstroke*2) + defaultsize;
+
+	//load the data and pre-calculate/set the values for each node 
+	nodedata.forEach(function(d) {			
+			d.scaling = d.relatedcount;
+			d.radius = ((defaultsize*d.scaling)/2); 
+			d.x = 0;
+			d.y = 0;
+			d.cx = d.x + d.radius;
+			d.cy = d.y + d.radius;
+			d.size = defaultsize * d.scaling;
+			d.totalsize = totalnodesize * d.scaling;
+		});	
 
 	var drawPane = d3.select("#"+selectid)
 		.style("fill","gray")
 		.on("keydown", keydown)
 		.on("keyup", keyup)
 		.on("click", clicked);
-
-	
 
 	var svg = drawPane.append("svg");
 
@@ -66,6 +77,19 @@ function drawGraph(selectid) {
 				zoomLayer.attr("transform", d3.event.transform);
 			}));	
 
+	//setup arrows for lines
+	svg.append('svg:defs')
+		.append('svg:marker')
+			.attr('id', 'end-arrow')
+			.attr('viewBox', '0 -5 10 10')
+			.attr('refX', 6)
+			.attr('markerWidth', 3)
+			.attr('markerHeight', 3)
+			.attr('orient', 'auto')
+			.append('svg:path')
+				.attr('d', 'M0,-5 L10,0 L0,5')
+				.attr('fill', "rgb(6,120,155)");
+
 	//setup the edges
 	var edges = zoomLayer.selectAll("line")
 		.data(linkdata)
@@ -73,7 +97,8 @@ function drawGraph(selectid) {
 		.append("line")
 		.attr("class","edges")
 		.style("stroke", "rgb(6,120,155)")
-		.style("stroke-width", 2);
+		.style("stroke-width", 2)
+		.style('marker-end', "url(#end-arrow)");
 
 	//build the nodes
 	var nodes = zoomLayer.selectAll(".nodes")
@@ -92,32 +117,37 @@ function drawGraph(selectid) {
 
 	//node layout
 	nodes.append("circle")
-		.attr("r", function(d) { return ((defaultsize * d.scaling)/2) + "px" })
-		.attr("cx", function(d) { return ((defaultsize * d.scaling)/2) })
-		.attr("cy", function(d) { return ((defaultsize * d.scaling)/2) })
-		.style("stroke-width", function(d) { return (defaultstroke * d.scaling) + "px" })
+		.attr("r", function(d) { return d.radius + "px"; })
+		.attr("cx", function(d) { return d.radius; })
+		.attr("cy", function(d) { return d.radius; })
+		.style("stroke-width", function(d) { return defaultstroke + "px" })
 		.attr("fill", function(d) { return d.fill })
 		.style("stroke", function(d) { return d.color });
 		
 
 	nodes.append("i")
-		.attr("height", function(d) { return ((defaultsize * d.scaling) * 0.6) + "px" })
-		.attr("width", function(d) { return ((defaultsize * d.scaling) * 0.6) + "px" })
-		.attr("x", function(d) { return ((defaultsize * d.scaling) * 0.2) })
-		.attr("y",function(d) { return ((defaultsize * d.scaling) * 0.2)})
+		.attr("height", function(d) { return ( d.size * 0.6) + "px" })
+		.attr("width", function(d) { return (d.size * 0.6) + "px" })
+		.attr("x", function(d) { return (d.size * 0.2) })
+		.attr("y",function(d) { return (d.size * 0.2)})
 		.attr("class", function(d) { return d.class })
 		.attr("color", function(d) { return d.color })
 		.classed("nodeicon",true); 
 
 	nodes.append("text")
-		.text(function(d) { return d.name; }); 
-	
+		.text(function(d) { return d.name; })
+		.attr("text-anchor","left")
+		.attr("font-size","10px")
+		.attr("font-family","arial")
+		.attr("transform",function(d) { return "translate(" + (d.totalsize + 2) 
+			+ "," + ((d.totalsize /2) + 2) + ")" }); 
+
 	//setup simulation/force layout
 	simulation.nodes(nodedata)
 		.force("link", d3.forceLink().id(function(d) { return d.id; }))
 		.force('charge', d3.forceManyBody().strength(-5)) 
 		.force('center', d3.forceCenter(640 / 2, 480 / 2))
-		.force('collision', d3.forceCollide().radius(function(d) { return ((defaultsize * d.scaling))}))
+		.force('collision', d3.forceCollide().radius(function(d) { return (totalnodesize)}))
 		.on('tick', function () {		
 			updateLocations();
 		});
@@ -193,11 +223,30 @@ function drawGraph(selectid) {
 	}
 
 	function updateLocations() {
-		edges.attr("x1", function(d) { return (d.source.x + ((defaultsize*d.source.scaling))/2)})
-			.attr("y1", function(d) { return (d.source.y + ((defaultsize*d.source.scaling))/2) })
-			.attr("x2", function(d) { return (d.target.x + ((defaultsize*d.target.scaling))/2)})
-			.attr("y2", function(d) { return (d.target.y + ((defaultsize*d.target.scaling))/2) });
+		edges.each(function(d) {
+			var sourceX= d.source.x + d.source.cx;
+			var sourceY = d.source.y + d.source.cy;
+			var targetX = d.target.x + d.target.cx;
+			var targetY = d.target.y + d.target.cy;
 
+			// Total difference in x and y from source to target
+			var diffX = targetX - sourceX;
+			var diffY = targetY - sourceY;
+
+			// Length of path from center of source node to center of target node
+			var pathLength = Math.sqrt((diffX * diffX) + (diffY * diffY));
+
+			///sin(angle) - some trig with angle taken opposite the 
+			var sinA = diffY / pathLength;
+			var cosA = diffX / pathLength;
+
+			d3.select(this)
+				.attr("x1", function () { return sourceX} )
+				.attr("y1", function () { return sourceY} )
+				.attr("x2", function(d) { return targetX - (cosA * (d.target.radius+6)); }) //the number adds a little padding
+				.attr("y2", function(d) { return targetY - (sinA * (d.target.radius+6)); } );
+		});
+			
 		nodes.attr("x",function(d) { return d.x; })
 			.attr("y",function(d) { return d.y; })
 			.attr("transform", function (d) { return "translate(" + d.x  + "," + d.y + ")" });	
@@ -225,5 +274,9 @@ function drawGraph(selectid) {
 				.attr("color", function(d) { return d.color; }); 
 	}
 }
+
+
+
+
 
 
