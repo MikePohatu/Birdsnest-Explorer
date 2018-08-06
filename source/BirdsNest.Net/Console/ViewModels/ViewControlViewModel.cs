aad696a9740895cdb2ab37c25ab77e9588de6667
Mike Pohatu <mikepohatu@gmail.com>
2018-08-06 22:15:28 +1200

    fixed tooltip not working at all. added generic controlview tooltip partial view

---
+++ source/BirdsNest.Net/Console/ViewModels/ViewControlViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Console.ViewModels
+{
+    public class ViewControlViewModel
+    {
+        public string SpanID { get; set; }
+        public string IconID { get; set; }
+        public string FaIcon { get; set; }
+        public string ToolTipText { get; set; }
+        public string Class { get; set; }
+    }
+}
