c6fd6022c3c1b4377bc72ada390fa9064cb6ec73
Mike Pohatu <mikepohatu@gmail.com>
2018-08-16 21:55:38 +1200

    added writetoneo module

--- source/utils/Citrix-AppsScanner.ps1
+++ source/utils/Citrix-AppsScanner.ps1
@@ -1,6 +1,10 @@
 #$creds = Get-Credential
 #-Credential $creds 
 
+Import-Module "$($PSScriptRoot)\WriteToNeo.psm1"
+$neoURL="http://localhost:7474/db/data/transaction/commit"
+$neoconf = "C:\birdsnest\Scanners\neoconfig.json"
+
 $zdc ='server'
 
 write-host "Querying $zdc"
@@ -51,55 +55,6 @@ Function Get-CitrixApps {
     write-host "Found $($resultobj.relationships.Count) relationships"
     return $resultobj
 }
-
-
-
-Function WriteToNeo {
-    Param (
-        [Parameter(Mandatory=$true)][string]$NeoConfigPath,
-        [Parameter(Mandatory=$true)][string]$Query,
-        [System.Collections.Generic.IDictionary[[string],[object]]]$Parameters
-    )
-    $results = $null
-
-    try {
-        $neoconfig = Get-Content -Raw -Path $NeoConfigPath | ConvertFrom-Json
-        $serverURL="http://localhost:7474/db/data/transaction/commit"
-
-        $secPasswd = ConvertTo-SecureString $neoconfig.DB_Password -AsPlainText -Force
-        $neo4jCreds = New-Object System.Management.Automation.PSCredential ($neoconfig.DB_Username, $secPasswd) 
-        $paramsjson = $Parameters | ConvertTo-Json
-
-        # Cypher query using parameters to pass in properties
-        $statement = new-object 'system.collections.generic.dictionary[[string],[object]]'
-        $statement.Add('statement',$Query)
-        $statement.Add('parameters',$Parameters)
-        $statements = new-object 'system.collections.generic.list[object]'
-        $statements.Add($statement)
-
-        #'{"statements" : [{' +
-        #           '"statement" : "' + $Query + '",' +
-        #           '"parameters" : ' + $paramsjson +
-        #           '}]' +
-        #        '}'    
-        $body = $statement = new-object 'system.collections.generic.dictionary[[string],[object]]'
-        $body.Add('statements',$statements)
-
-        $bodyjson = $body |ConvertTo-Json -Depth 5
-
-        
-        # Call Neo4J HTTP EndPoint, Pass in creds & POST JSON Payload
-        $response = Invoke-WebRequest -Uri $serverURL -Method POST -Body $bodyjson -credential $neo4jCreds -ContentType "application/json"
-
-    } 
-    finally {
-      
-    }
-    return $response
-}
-
-
-
 
 
 $resultsobj = Get-CitrixApps -DataCollector $zdc
