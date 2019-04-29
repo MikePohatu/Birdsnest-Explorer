db777f914c0e5e78a1159e0b1be90b3a4261c7f5
Mike Pohatu <mike@20road.com>
2019-04-29 22:21:37 +1200

    more search buildout

---
+++ source/BirdsNest.Net/Console/neo4jProxy/SearchCondition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Console.neo4jProxy
+{
+    public class SearchCondition
+    {
+        public string Property { get; set; }
+        public string Value { get; set; }
+        public bool CaseSensitive { get; set; } = false;
+        public bool UseWildCards { get; set; } = false;
+    }
+}
--- source/BirdsNest.Net/Console/neo4jProxy/SearchNode.cs
+++ source/BirdsNest.Net/Console/neo4jProxy/SearchNode.cs
@@ -7,14 +7,21 @@ namespace Console.neo4jProxy
 {
     public class SearchNode
     {
-        public string Type { get; set; }
-        public string Property { get; set; }
-        public string Value { get; set; }
+        
 
-        public override string ToString()
+        public string Identifier { get; set; }
+        public string Type { get; set; }
+
+        public List<SearchCondition> Conditions { get; private set; } = new List<SearchCondition>();
+
+        public string NodeString()
+        {
+            return string.IsNullOrEmpty(this.Type) ? "(" + this.Identifier + ")" : "(" + this.Identifier + ":" + this.Type + ")";
+        }
+
+        public string WhereString()
         {
             StringBuilder builder = new StringBuilder();
-
             return builder.ToString();
         }
     }
