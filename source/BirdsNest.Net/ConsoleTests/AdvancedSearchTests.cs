6bcee2a89cfc7d8fe91102b3d7f637209e5d4ddc
Mike Pohatu <mike@20road.com>
2019-08-09 22:00:07 +1200

    additional test

--- source/BirdsNest.Net/ConsoleTests/AdvancedSearchTests.cs
+++ source/BirdsNest.Net/ConsoleTests/AdvancedSearchTests.cs
@@ -106,6 +106,43 @@ namespace ConsoleTests
     }
 }",
             ExpectedResult = "(n1.tpropn1 < 1 AND (n2.tpropn2 STARTS WITH 'testn2' OR r1.tpropr1 = 'testr1'))")]
+        [TestCase(@"{
+    ""Condition"": {
+        ""Type"": ""AND"",
+        ""Conditions"": [{
+            ""Type"": ""MATH"",
+            ""Name"": ""n1"",
+            ""Property"": ""tpropn1"",
+            ""Value"": 1,
+            ""Operator"": ""<""
+        },
+        {
+            ""Type"": ""OR"",
+            ""Conditions"": [{
+                ""Type"": ""STRING"",
+                ""Name"": ""n2"",
+                ""Property"": ""tpropn2"",
+                ""Value"": ""testn2"",
+                ""Comparator"": ""STARTSWITH""
+            },
+            {
+                ""Type"": ""STRING"",
+                ""Name"": ""r1"",
+                ""Property"": ""tpropr1"",
+                ""Value"": ""testr1"",
+                ""Comparator"": ""EQUALS""
+            },
+            {
+                ""Type"": ""STRING"",
+                ""Name"": ""r2"",
+                ""Property"": ""tpropr2"",
+                ""Value"": ""testr2"",
+                ""Comparator"": ""EQUALS""
+            }]
+        }]
+    }
+}",
+            ExpectedResult = "(n1.tpropn1 < 1 AND (n2.tpropn2 STARTS WITH 'testn2' OR r1.tpropr1 = 'testr1' OR r2.tpropr2 = 'testr2'))")]
         public string GenerateSearchStringTest(string json)
         {
             Search s = JsonConvert.DeserializeObject<Search>(json);
