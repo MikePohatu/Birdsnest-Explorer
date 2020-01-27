dfe4c095282584496ad73980d4a75facb9de9c36
Mike Pohatu <mike@20road.com>
2020-01-28 09:36:42 +1300

    started azureadscanner

---
+++ source/BirdsNest.Net/AzureADScanner/Configuration.cs
@@ -0,0 +1,57 @@
+﻿#region license
+// Copyright (c) 2019-2020 "20Road"
+// 20Road Limited [https://20road.com]
+//
+// This file is part of BirdsNest.
+//
+// BirdsNest is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+#endregion
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureADScanner
+{
+    public class Configuration : IDisposable
+    {
+        [JsonProperty("ScannerID")]
+        public string ScannerID { get; set; }
+
+        [JsonProperty("Tenant")]
+        public string Tenant { get; set; }
+
+        [JsonProperty("Key")]
+        public string Key { get; set; }
+
+        public static Configuration LoadConfiguration(string filepath)
+        {
+            Configuration conf;
+            using (StreamReader file = File.OpenText(filepath))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                conf = (Configuration)serializer.Deserialize(file, typeof(Configuration));
+            }
+            return conf;
+        }
+
+        public void Dispose()
+        {
+            this.Key = string.Empty;
+        }
+    }
+}
---
+++ source/BirdsNest.Net/AzureADScanner/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using common;
+using Neo4j.Driver.V1;
+
+namespace AzureADScanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch steptimer = new Stopwatch();
+            Stopwatch totaltimer = new Stopwatch();
+            string _appdir = AppDomain.CurrentDomain.BaseDirectory;
+            string neoconfigfile = _appdir + @"\neoconfig.json";
+            string configfile = _appdir + @"\cmconfig.json";
+            bool batchmode = false;
+            string scanid = ShortGuid.NewGuid().ToString();
+            string scannerid = string.Empty;
+
+            IDriver driver = null;
+
+            totaltimer.Start();
+            try
+            {
+                foreach (string arg in args)
+                {
+                    string[] param = arg.Split(new[] { ":" }, 2, StringSplitOptions.None);
+                    switch (param[0].ToUpper())
+                    {
+                        case "/?":
+                            ShowUsage();
+                            Environment.Exit(0);
+                            break;
+                        case "/CONFIG":
+                            configfile = param[1];
+                            break;
+                        case "/BATCH":
+                            batchmode = true;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("There is a problem with arguments: " + string.Join(" ", args));
+                Console.WriteLine("");
+                ShowUsage();
+                Environment.Exit(1);
+            }
+
+            //load the config
+            try
+            {
+                using (Configuration config = Configuration.LoadConfiguration(configfile))
+                {
+                    scannerid = config.ScannerID;
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("There was an error connecting to the server with your configuration");
+                Console.WriteLine(e.Message);
+                if (batchmode == false) { Console.ReadLine(); }
+                Environment.Exit(1);
+            }
+
+            try
+            {
+                using (NeoConfiguration config = NeoConfiguration.LoadConfigurationFile(neoconfigfile))
+                {
+                    driver = Neo4jConnector.ConnectToNeo(config);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("There was an error loading your neo4j configuration");
+                Console.WriteLine(e.Message);
+                if (batchmode == false) { Console.ReadLine(); }
+                Environment.Exit(2);
+            }
+
+            NeoWriter.ScanID = scanid;
+
+            //cleanup
+
+            if (batchmode == true)
+            {
+                Console.Write("Exiting.");
+                for (int i = 0; i < 3; i++)
+                {
+                    System.Threading.Thread.Sleep(500);
+                    Console.Write(".");
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Press any key to exit");
+                Console.ReadLine();
+            }
+        }
+
+        private static void ShowUsage()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Usage: AzureADScanner.exe /config:<configfile> /batch");
+            Console.WriteLine("/batch makes scanner run in batch mode and does not wait before exit");
+        }
+    }
+}
