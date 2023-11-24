b3bf22d002471808323b0e048b8f03b1a05af1a4
Mike Pohatu <mike@20road.com>
2023-11-24 14:13:25 +1300

    release | 1.1.31124.0

--- source/BirdsNest.Net/Attributed/Properties/AssemblyInfo.cs
+++ source/BirdsNest.Net/Attributed/Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("20Road Limited")]
 [assembly: AssemblyProduct("Birdsnest Explorer")]
-[assembly: AssemblyCopyright("Copyright © 20Road Limited, 2020")]
+[assembly: AssemblyCopyright("Copyright ©20Road Limited 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,5 +49,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.31124.0")]
+[assembly: AssemblyFileVersion("1.1.31124.0")]
